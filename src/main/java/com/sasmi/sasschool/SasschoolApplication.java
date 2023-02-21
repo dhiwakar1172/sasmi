da3e56ae465761fdb4a67076b7b3dd80c5b1e753
dhiwakar1172 <107527878+dhiwakar1172@users.noreply.github.com>
2023-02-21 22:53:47 +0530

    Add files via upload

---
+++ src/main/java/com/sasmi/sasschool/SasschoolApplication.java
@@ -0,0 +1,13 @@
+package com.sasmi.sasschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SasschoolApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sasmi/sasschool/model/Holiday.java
@@ -0,0 +1,15 @@
+package com.sasmi.sasschool.model;
+
+import lombok.Data;
+
+@Data
+public class Holiday {
+
+    private final String day;
+    private final String reason;
+    private final Type type;
+
+    public enum Type {
+        FESTIVAL, FEDERAL
+    }
+}
